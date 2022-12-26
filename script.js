09895822ea0ac1ef31ff2705cba227aec84fcd69
simplejeeva <skyplayjeeva@gmail.com>
2022-12-26 12:18:16 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+ let outputscreen=document.getElementById("output-screen");
+
+ function display(num){
+    outputscreen.value +=num
+ }
+ function calculate (){
+    try{
+        outputscreen.value = eval(outputscreen.value);
+    }
+    catch(err){
+        alert("invalid")
+    }
+  
+ }
+ function Clear(){
+    outputscreen.value=" ";
+}
+ function del(){
+    outputscreen.value=outputscreen.value.slice(0,-1)
+ }
